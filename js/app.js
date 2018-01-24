c24e40b23817b654fb970bcb2507d4490100c119
Jess Moran <j-es-itza@hotmail.com>
2018-01-24 06:44:41 -0600

    agregando num aleatorio

--- js/app.js
+++ js/app.js
@@ -1,13 +1,13 @@
 var $numberInput = $("#number");
-
+var $containerNumber = $("#next");
+var $addCode = $("#code");
 
 function loadPage (){
   $numberInput.keyup(validateNumber);
+  $containerNumber.click(getCode);
 }
 
-
 function validateNumber () {
-  var $containerNumber = $("#next");
 
   if($(this).val().trim().length > 0) {
     $containerNumber.removeAttr("disabled");
@@ -15,6 +15,16 @@ function validateNumber () {
     $containerNumber.attr("disabled" , true);
   }
 }
+
+
+
+function getCode() {
+  var $code = (Math.floor(Math.random(100) * Math.floor(900)));
+  $addCode.text("LAB-" + $code);
+
+}
+
+
 
 
 $(document).ready(loadPage);
