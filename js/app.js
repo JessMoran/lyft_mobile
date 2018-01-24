cdb5d2a8d63c3750a7e3045cdb6f19fe34e6cafb
Jess Moran <j-es-itza@hotmail.com>
2018-01-24 07:43:51 -0600

    activando boton al agregar codigo

--- js/app.js
+++ js/app.js
@@ -1,29 +1,49 @@
-var $numberInput = $("#number");
-var $containerNumber = $("#next");
-var $addCode = $("#code");
+//variables globales en uso para todas las funciones
 
+var $numberInput = $("#number");
+var $btnNext = $("#next");
+var $addCode = $("#code");
+var $codeInput = $("#newCode")
+var $btnNext2 = $("#next2");
+
+//funciona al cargar la página
 function loadPage (){
   $numberInput.keyup(validateNumber);
-  $containerNumber.click(getCode);
+  $btnNext.click(getCode);
+  $codeInput.keyup(validateCode);
 }
 
+//Funciones primer vista
+//Valida que no tenga espacios y activa el boton next
 function validateNumber () {
 
   if($(this).val().trim().length > 0) {
-    $containerNumber.removeAttr("disabled");
+    $btnNext.removeAttr("disabled");
   } else {
-    $containerNumber.attr("disabled" , true);
+    $btnNext.attr("disabled" , true);
   }
 }
 
-
+//función que da un numero aleatorio de 3 digitos
+//variable global para usar $code en la sig función
+  var $code = (Math.floor(Math.random(100) * Math.floor(999)));
 
 function getCode() {
-  var $code = (Math.floor(Math.random(100) * Math.floor(900)));
   $addCode.text("LAB-" + $code);
+}
+//!Funciones primer vista
+
+//funciones Segundavista
+//Valida si el código es igual al enviado anteriormente
+function validateCode (){
+
+ if ($(this).val().trim().length > 0) {
+   $btnNext2.removeAttr("disabled");
+ } else {
+   $btnNext2.attr("disabled" , true);
+ }
 
 }
-
 
 
 
